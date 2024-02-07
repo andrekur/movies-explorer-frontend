2d734b2bafaa955f5fc4d30d87520fcc36d7b3d0
andre <kurepin.andre@yandex.ru>
2024-02-07 17:41:56 +0300

    Добавляет логи с возвратом

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function NotFoundPage() {
+
+  function handleBackward(e) {
+    e.preventDefault();
+
+    window.history.go(-1)
+  }
+
   return (
     <section className="not-found-page">
       <h1 className="not-found-page__title">404</h1>
       <p className="not-found-page__text">Страница не найдена</p>
-      <a className="not-found-page__return-link" href="/">Назад</a>
+      <a className="not-found-page__return-link" onClick={handleBackward}>Назад</a>
     </section>
   )
 }
