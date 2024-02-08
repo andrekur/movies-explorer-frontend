387bbd155da085d700d368100ef67c1b10478817
andre <kurepin.andre@yandex.ru>
2024-02-08 16:11:44 +0300

    Изменяет блок ссылок

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -13,7 +13,7 @@ function AboutMe() {
           <p className="aboutme__history">
           Я живу в мегаполисе Москва, закончил технический факультет в РТУ МИРЭА. Домашних животных нет, но в планах завести любимого кота. Я люблю слушать музыку и читать крутые книжки, а ещё увлекаюсь программированием. Пишу код на Python еще со школы. В 2023 году начал осваивать для себе новое направлении&nbsp;&mdash;&nbsp;Frontend. После окончания курса планирую развиваться как Fullstack.
           </p>
-          <a className="aboutme__link" href="https://github.com/andrekur">Github</a>
+          <a className="aboutme__link" href="https://github.com/andrekur" rel="noreferrer" target="_blank">Github</a>
         </div>
         <img className="aboutme__author-image" src={Avatar} alt="Фотография автора"/>
       </div>
--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -5,17 +5,14 @@ function Portfolio() {
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__links">
-        <li className="portfolio__link">
-          <a className="portfolio__link-text" href="https://andrekur.github.io/how-to-learn/" rel="noreferrer" target="_blank">Статичный сайт</a>
-          <div className="portfolio__link-img"/>
+        <li>
+          <a className="portfolio__link" href="https://andrekur.github.io/how-to-learn/" rel="noreferrer" target="_blank">Статичный сайт</a>
         </li>
-        <li className="portfolio__link">
-          <a className="portfolio__link-text" href="https://andrekur.github.io/russian-travel/" rel="noreferrer" target="_blank">Адаптивный сайт</a>
-          <div className="portfolio__link-img"/>
+        <li>
+          <a className="portfolio__link" href="https://andrekur.github.io/russian-travel/" rel="noreferrer" target="_blank">Адаптивный сайт</a>
         </li>
-        <li className="portfolio__link portfolio__link_border-unactive">
-          <a className="portfolio__link-text" href="https://andrekur.nomoredomainsmonster.ru" rel="noreferrer" target="_blank">Одностраничное приложение</a>
-          <div className="portfolio__link-img"/>
+        <li>
+          <a className="portfolio__link" href="https://andrekur.nomoredomainsmonster.ru" rel="noreferrer" target="_blank">Одностраничное приложение</a>
         </li>
       </ul>
     </section>
