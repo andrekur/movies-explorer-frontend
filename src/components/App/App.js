db3c9e1be7eae9855e983a432560da1bbbae5793
andre <kurepin.andre@yandex.ru>
2024-02-08 17:39:01 +0300

    Завершает 1280/768

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="page">
-        <Header loggedIn={true}/>
+        <Header loggedIn={false}/>
         <main>
           <Routes>
             <Route path="*" element={<NotFoundPage/>}></Route>
